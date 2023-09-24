a04735106a803a238704a50f71c48aa31f1d0a87
roman-ojha <razzroman98@gmail.com>
2023-09-24 09:07:50 +0545

    feat: run bun server on first bun project

--- Notes/01_Main/01_Init/index.ts
+++ Notes/01_Main/01_Init/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const server = Bun.serve({
+  port: 8080,
+  fetch(req) {
+    console.log(Bun.version);
+    const body = figlet.textSync("Bun!");
+    // return new Response("Bun is running!");
+    return new Response(body);
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port}`);
